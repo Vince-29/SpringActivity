082faa75f9a45477561eb95e28729564daa4d7ad
bansannn <vincent.olpate@unc.edu.ph>
2024-11-22 00:02:52 +0800

    creation of user class

--- UserProfile/src/main/java/test/userprofile/UserProfileApplication.java
+++ UserProfile/src/main/java/test/userprofile/UserProfileApplication.java
@@ -2,12 +2,18 @@ package test.userprofile;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @SpringBootApplication
 public class UserProfileApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(UserProfileApplication.class, args);
+    }
+
+    @RequestMapping
+    public String index() {
+        return "HELLO WORLD";
     }
 
 }
